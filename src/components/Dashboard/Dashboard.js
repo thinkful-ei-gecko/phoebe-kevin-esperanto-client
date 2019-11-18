3fab1f51ad138c71265e5c17e4c3ab9f122d3176
Kevin Wei <chenghaowei@gmail.com>
2019-11-18 18:50:16 -0500

    add key attribute, change blogful back to esperanto in config

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,35 +1,36 @@
-import React, { Component } from "react";
-import Button from "../Button/Button";
-import languageContext from "../../contexts/LanguageContext";
+import React, { Component } from 'react';
+import Button from '../Button/Button';
+import languageContext from '../../contexts/LanguageContext';
 // import { Link } from "react-router-dom";
-import "./Dashboard.css";
+import './Dashboard.css';
 
 export default class Dashboard extends Component {
-	static contextType = languageContext;
+  static contextType = languageContext;
 
-	render() {
-		const { language, words } = this.context;
+  render() {
+    const { language, words } = this.context;
 
-		return (
-			<>
-				<h3>Words to Practice</h3>
-				<ul>
-					{words.map(word => {
-						return (
-						<li><span className='bold'>{word.original}</span>
-							<ul>
-								<li>correct answer count: {word.correct_count}</li>
-								<li>incorrect answer count: {word.incorrect_count}</li>
-							</ul>
-						</li>
-						)
-					})}
-				</ul>
-				<h3>Total correct answers: {language.total_score}</h3>
-				<a href='/learn'>
-					<Button>Start practicing!</Button>
-				</a>
-			</>
-		);
-	}
+    return (
+      <>
+        <h3>Words to Practice</h3>
+        <ul>
+          {words.map((word) => {
+            return (
+              <li key={word.id}>
+                <span className="bold">{word.original}</span>
+                <ul>
+                  <li>correct answer count: {word.correct_count}</li>
+                  <li>incorrect answer count: {word.incorrect_count}</li>
+                </ul>
+              </li>
+            );
+          })}
+        </ul>
+        <h3>Total correct answers: {language.total_score}</h3>
+        <a href="/learn">
+          <Button>Start practicing!</Button>
+        </a>
+      </>
+    );
+  }
 }
