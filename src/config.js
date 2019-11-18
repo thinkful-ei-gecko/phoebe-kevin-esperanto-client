c12555dd59c893f114c810a8df45a77560f0eeae
Kevin Wei <chenghaowei@gmail.com>
2019-11-18 17:55:16 -0500

    add in localhost endpoint for local testing

--- src/config.js
+++ src/config.js
@@ -1,4 +1,7 @@
 export default {
-  REACT_APP_API_ENDPOINT: process.env.REACT_APP_API_ENDPOINT,
-  TOKEN_KEY: 'blogful-client-auth-token',
-}
+  REACT_APP_API_ENDPOINT:
+    process.env.NODE_ENV === 'production'
+      ? process.env.REACT_APP_API_ENDPOINT
+      : 'http://localhost:8000/api',
+  TOKEN_KEY: 'esperanto-client-auth-token',
+};
