fcc6785a5df359eddd72b24cbd518fe1f4f2a7f7
Kevin Wei <chenghaowei@gmail.com>
2019-11-18 19:15:23 -0500

    move componentDidMount to Dashboard component, add setState method to context

--- src/contexts/LanguageContext.js
+++ src/contexts/LanguageContext.js
@@ -1,44 +1,36 @@
 //Use to store language and words in the response from the API request
 
-import React, { Component } from 'react'
-import languageApiService from '../services/language-api-service'
+import React, { Component } from 'react';
 
-const LanguageContext = React.createContext({
-  language: '',
-  words: [], 
-})
+const LanguageContext = React.createContext({});
 
-export default LanguageContext
+export default LanguageContext;
 
 export class LanguageProvider extends Component {
-
   state = {
     language: '',
     words: [],
-  }
+  };
 
-  componentDidMount() {
-    languageApiService.getLanguageAndWords()
-      .then(res => {
-        const { language, words } = res;
-        this.setState({
-          language,
-          words
-        })
-      })
-  }
+  setLanguageAndWords = (language, words) => {
+    this.setState({
+      language,
+      words,
+    });
+  };
 
   render() {
     const value = {
       language: this.state.language,
       words: this.state.words,
       totalScore: this.state.totalScore,
-    }
+      setLanguageAndWords: this.setLanguageAndWords,
+    };
 
     return (
       <LanguageContext.Provider value={value}>
         {this.props.children}
       </LanguageContext.Provider>
-    )
+    );
   }
 }
--- src/routes/DashboardRoute/DashboardRoute.js
+++ src/routes/DashboardRoute/DashboardRoute.js
@@ -1,16 +1,23 @@
-import React, { Component } from 'react'
-import Dashboard from '../../components/Dashboard/Dashboard'
-import languageContext from '../../contexts/LanguageContext'
+import React, { Component } from 'react';
+import Dashboard from '../../components/Dashboard/Dashboard';
+import languageContext from '../../contexts/LanguageContext';
+import languageApiService from '../../services/language-api-service';
 
 class DashboardRoute extends Component {
-  static contextType = languageContext
+  static contextType = languageContext;
+
+  componentDidMount = () => {
+    languageApiService.getLanguageAndWords().then((res) => {
+      const { language, words } = res;
+      this.context.setLanguageAndWords(language, words);
+    });
+  };
 
   render() {
     const { language, words } = this.context;
-		
-		if (!language || words.length === 0 ) {
-			return null;
-		} else {
+    if (!language || words.length === 0) {
+      return null;
+    } else {
       return (
         <section>
           <h2>{language.name} Progress</h2>
