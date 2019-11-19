6902fae637a490a334ed7f3d0650770b9d68c8c9
Phoebe Law <phoebebasilio@gmail.com>
2019-11-18 18:15:22 -0800

    Passes cypress tests for day 2

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -14,9 +14,9 @@ export default class Dashboard extends Component {
       <>
         <h3>Words to practice</h3>
         <ul>
-				{words.map((word, index) => {
+				{words.map(word => {
             return (
-              <li key={index}>
+              <li key={word.id}>
                 <h4>{word.original}</h4>
 								<p>correct answer count: {word.correct_count}</p>
 								<p>incorrect answer count: {word.incorrect_count}</p>
